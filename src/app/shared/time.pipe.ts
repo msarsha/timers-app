43af6b0b88dc8b6f5ef82b4dab3c5e2cc636b3b1
Matan Sar-Shalom <smatan84@gmail.com>
2021-07-28 21:42:11 +0300

    feat(remaining-time): add test and no time string

--- src/app/shared/time.pipe.ts
+++ src/app/shared/time.pipe.ts
@@ -1,10 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const NO_TIME_STRING = '--:--:--';
+
 @Pipe({
   name: 'time'
 })
 export class TimePipe implements PipeTransform {
-  transform(value: number|null): string {
-    return new Date(1000 * (value || 0)).toISOString().substr(11, 8)
+  transform(value: number|null, showNoTimeString = false): string {
+    if(value === 0 && showNoTimeString) {
+      return NO_TIME_STRING;
+    }
+
+    return new Date(1000 * (value || 0)).toISOString().substr(11, 8);
   }
 }
